9a6dd8de8af7d14f2ea29af7beda3e99abf365e9
tomer2b <60958579+tomer2b@users.noreply.github.com>
2024-01-13 18:29:39 +0200

    Update auto_gradeing.py

--- auto_gradeing.py
+++ auto_gradeing.py
@@ -1,32 +1,6 @@
-import builtins as __builtin__
 import pandas as pd
-
-run = None
-
-def print(*args,  **kwargs):
-
-    if run != None and run.test_mode==True:
-        with open('output.txt', "w") as out:
-            __builtin__.print(*args, **kwargs, file=out)
-        with open('output.txt', "r") as out:
-            txt_batch = out.readline().strip()
-        run.output_lst.append(txt_batch)
-        return txt_batch
-    else:
-        txt = __builtin__.print(*args, **kwargs)
-        return txt
-
-def input(prompt=None):
-    if run != None and run.test_mode==True:
-        batch_input = run.input_lst[run.input_counter]
-        run.input_counter += 1
-    else:
-        batch_input = __builtin__.input(prompt)
-    return batch_input
-
-def set_run(new_val):
-    global run
-    run=new_val
+import urllib.request
+import builtins as __builtin__
 
 
 
@@ -48,6 +22,35 @@ def import_tasks(grade,exercise):
     t.append(sublist)
   return t
 
+run = None
+print('autograde before update',id(run),id(print))
+def print(*args,  **kwargs):
+
+    if run != None and run.test_mode==True:
+        with open('output.txt', "w") as out:
+            __builtin__.print(*args, **kwargs, file=out)
+        with open('output.txt', "r") as out:
+            txt_batch = out.readline().strip()
+        run.output_lst.append(txt_batch)
+        return txt_batch
+    else:
+        txt = __builtin__.print(*args, **kwargs)
+        return txt
+
+def input(prompt=None):
+    if run != None and run.test_mode==True:
+        batch_input = run.input_lst[run.input_counter]
+        run.input_counter += 1
+    else:
+        batch_input = __builtin__.input(prompt)
+    return batch_input
+
+# def set_run(new_val):
+#     global run
+#     run=new_val
+
+
+
 class CheckAssignment:
 
     def __init__(self):
@@ -55,14 +58,11 @@ class CheckAssignment:
         self.input_counter = 0
         self.input_lst = []
         self.output_lst = []
-        self.run_results = {}
+        self.run_result = {}
         # self.runs = tests
 
 
 
-    def print_results(self):
-        for key, val in self.run_results.items():
-            print(key, val)
 
     def run_task(self,func, parms, in_list, expected_result, result_type):
         try:
@@ -90,14 +90,31 @@ class CheckAssignment:
             func_call = func + '(' + str(parms)[1:-1] + ')'
             return False, func_call, e
 
-    def run_test(self,tasks,student_functions):
-        for k,v in student_functions.items():
-            if globals().get(k)==None:
-                globals()[k]=v
-        # position4 is 1 for  print , 2 for return
-        ex_count = 0
-        self.test_mode = True
-        for i in range(len(tasks)):
-            self.run_results[ex_count] = self.run_task(tasks[i][0], tasks[i][1], tasks[i][2], tasks[i][3], tasks[i][4])
-            ex_count += 1
-        self.test_mode = False
+def run_test(tasks,student_functions):
+    correct_answer = 0
+    run_results = {}
+    for k,v in student_functions.items():
+        if globals().get(k)==None:
+            globals()[k]=v
+
+    # position4 is 1 for  print , 2 for return
+    ex_count = 0
+    global run
+    run=CheckAssignment()
+
+    for i in range(len(tasks)):
+        run.test_mode = True
+        run_results[ex_count] = run.run_task(tasks[i][0], tasks[i][1], tasks[i][2], tasks[i][3], tasks[i][4])
+        run.test_mode = False
+        if run_results[ex_count][0]==True:
+            correct_answer+=1
+            print(f'Ok {tasks[i][0]}({tasks[i][1]})')
+        else:
+            print(f'X  {tasks[i][0]}({tasks[i][1]})', '\n\t\tinputs:        ', tasks[i][2], '\n\t\texpected print:', tasks[i][3], '\n\t\tactual output: ',run.output_lst )
+
+
+        ex_count += 1
+    print('----------')
+    print('grade:',round(100 * correct_answer / len(run_results)))
+
+
