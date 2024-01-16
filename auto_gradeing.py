78f5f899fca8deac9b8fd3793ce1363ccaabe7a2
tomer2b <60958579+tomer2b@users.noreply.github.com>
2024-01-16 14:49:52 +0200

    Update auto_gradeing.py

--- auto_gradeing.py
+++ auto_gradeing.py
@@ -6,7 +6,7 @@ import builtins as __builtin__
 
 def import_tasks(grade,exercise):
   t=[]
-  df = pd.read_csv('./tasks.csv',sep=',',on_bad_lines='skip',encoding='iso8859_8')
+  df = pd.read_csv('./tasks.csv',sep=',',on_bad_lines='skip',encoding='utf-8',encoding_errors='ignore')
 
   df = df[df['class']==grade]
   df = df[df['exercise']==exercise]
